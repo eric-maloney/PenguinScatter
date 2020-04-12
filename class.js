60f92cd0955efb035e5eaccbd35564fc0643bb4a
unknown <eric,maloney@centre.edu>
2020-04-12 15:00:59 -0400

    scatterlab2

--- class.js
+++ class.js
@@ -29,6 +29,33 @@ var meanHomework = function(penguin)
     return d3.mean (getHomework(penguin));
 }
 
+var getQuizzes = function(penguin)
+{
+    return penguin.quizes.map(getQuizgrade);
+}
+var getQuizgrade = function(quiz)
+{
+    return quiz.grade
+}
+var meanQuiz = function(penguin)
+{
+    return d3.mean (getQuizzes(penguin));
+}
+
+var getTests = function(penguin)
+{
+    return penguin.test.map(getTestgrade);
+}
+var getTestgrade = function(test)
+{
+    return test.grade
+}
+var meanTest = function(penguin)
+{
+    return d3.mean (getTests(penguin));
+}
+
+
 var getFinal = function(penguin)
 {
     return penguin.final[0].grade
@@ -50,17 +77,17 @@ var displaySpread = function(penguins)
     
     var xScale = d3.scaleLinear()
                 .domain([
-                    d3.min(penguins,getFinal),
-                    d3.max(penguins,getFinal)
+                    d3.min(penguins,getFinal)-10,
+                    d3.max(penguins,getFinal)+10
                 ])
-                .range([0,525])
+                .range([0,550])
     
     var yScale = d3.scaleLinear()
                 .domain([
-                          d3.min(penguins,meanHomework),
-                          d3.max(penguins,meanHomework)
+                          d3.min(penguins,meanHomework)-10,
+                          d3.max(penguins,meanHomework)+10
                         ])
-                .range([250,0]);
+                .range([300,0]);
     
     
                         
@@ -78,7 +105,73 @@ var displaySpread = function(penguins)
         })
         
         .attr("r",3)
-        .attr("fill","blue")
+        .attr("fill","blue") 
+    
     
 }
+
+var clearGraph = function()
+{
+    d3.selectAll("svg circle")
+        .remove();
+};
+
+
+  var buttons= function(penguins)
+  {
+     d3.select("#FvHW")
+      .on("clicked", function()
+          {console.log("clicked")
+           
+           
+           
+        .selectAll("circle")
+        .data(penguins)
+        .enter()
+        .append("circle")
+        .attr("cx",function(penguin)
+        {
+            return xScale(meanQuiz(penguin));    
+        })
+        .attr("cy",function(penguin)
+        {
+            return yScale(meanHomework(penguin));  
+        })
+        
+        .attr("r",3)
+        .attr("fill","blue") 
+           
+           
+           
+           
+        clearGraph()
+        displaySpread(penguins)
+        console.log("spread was drawn")
+        console.log(penguins)
+          });
+      
+ 
   
+     d3.select("#HWvQ")
+      .on("clicked", function()
+          {console.log("clicked");
+        
+          });
+      
+  
+
+     d3.select("#TvF")
+      .on("clicked", function()
+          {console.log("clicked");
+        
+          });
+      
+  
+
+     d3.select("#TvQ")
+      .on("clicked", function()
+          {console.log("clicked");
+        
+          });
+      
+  };
